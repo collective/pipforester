a1cb803fcf365784675b224d496a83073f1f325a
Jens W. Klein <jk@kleinundpartner.at>
2023-02-09 16:05:03 +0100

    add option to detect cycles and exit with 1 if detected

--- pipforester/__init__.py
+++ pipforester/__init__.py
@@ -7,12 +7,21 @@ import json
 @click.command()
 @click.option("--input", "-i", type=click.File("r"))
 @click.option("--output", "-o")
-# @click.option("--cycles", "-c")
-def main(input, output):
+@click.option("--cycles", is_flag=True)
+@click.option("--check-cycles", is_flag=True)
+def main(input, output, cycles, check_cycles):
     deptreedata = json.load(input)
-    G = deptree.graph_from_json(deptreedata)
-    bad_edges = deptree.detect_cyclic_edges(G)
-    deptree.remove_cyclic_edges(G, bad_edges)
-    deptree.remove_direct_edges(G)
-    deptree.add_cyclic_edges(G, bad_edges)
-    deptree.write_dotfile(G, output)
+    graph = deptree.graph_from_json(deptreedata)
+    if check_cycles:
+        bad_edges = deptree.detect_cyclic_edges(graph)
+        if bad_edges:
+            print("Cyclic dependencies detected")
+            exit(1)
+    elif cycles:
+        graph = deptree.extract_cyclic_graph(graph)
+    else:
+        bad_edges = deptree.detect_cyclic_edges(graph)
+        deptree.remove_cyclic_edges(graph, bad_edges)
+        deptree.remove_direct_edges(graph, ignore=bad_edges)
+        deptree.add_cyclic_edges(graph, bad_edges)
+    deptree.write_dotfile(graph, output)
--- pipforester/deptree.py
+++ pipforester/deptree.py
@@ -27,13 +27,30 @@ def graph_from_json(data):
     return G
 
 
-def detect_cyclic_edges(G):
+def detect_cyclic_edges(G, mark=False):
     print("Detecting cyclic edges")
     bad_edges = set()
     for cycle in nx.simple_cycles(G):
+        if mark:
+            for idx in range(len(cycle) - 1):
+                G.edges[cycle[idx], cycle[idx + 1]]["color"] = "darkviolet"
         bad_edges.add((cycle[-1], cycle[0]))
     return bad_edges
 
+
+def extract_cyclic_graph(G):
+    print("Extracting cyclic edges")
+    CG = nx.DiGraph()
+    bad_edges = set()
+    for num, cycle in enumerate(nx.simple_cycles(G)):
+        if (cycle[-1], cycle[0]) in bad_edges:
+            continue
+        for idx in range(len(cycle) - 1):
+            CG.add_edge(cycle[idx], cycle[idx + 1])
+            CG.add_edge(f"{cycle[idx]} ({num})", f"{cycle[idx + 1]} ({num})")
+        CG.add_edge(f"{cycle[-1]} ({num})", f"{cycle[0]} ({num})")
+        bad_edges.add((cycle[-1], cycle[0]))
+    return CG
 
 def remove_cyclic_edges(G, bad_edges):
     print("Removing cyclic edges")
@@ -44,17 +61,22 @@ def remove_cyclic_edges(G, bad_edges):
 def add_cyclic_edges(G, bad_edges):
     print("Adding cyclic edges")
     for edge in bad_edges:
-        G.add_edge(edge[0], edge[1], color="red")
+        G.add_edge(edge[0], edge[1], color="darkviolet")
 
 
-def remove_direct_edges(G):
+def remove_direct_edges(G, ignore=None):
     print("Removing direct edges")
     to_remove = []
+    if ignore is None:
+        ignore = []
     for node in G.nodes():
         # get all edges origin in node
         print(f"  Node: {node}")
         out_edges = list(G.out_edges(node))
         for out_edge in out_edges:
+            if tuple(out_edge) in ignore:
+                print(f"  Ignore cyclic: {node}")
+                continue
             print(f"    Out edge to {out_edge[1]}")
             for path in nx.all_simple_paths(G, node, out_edge[1]):
                 # if there is a path from node to out_edge[1] that is longer than 1, then
